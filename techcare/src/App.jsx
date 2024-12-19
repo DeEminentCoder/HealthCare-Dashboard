b388f3b8b720af3d4bba2cb09a4def245eb62d22
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-12-19 17:51:48 +0100

    create nav

--- techcare/src/App.jsx
+++ techcare/src/App.jsx
@@ -1,5 +1,8 @@
 // import React from 'react'
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar"
 
 const App = () => {
--- techcare/src/Components/Navbar.jsx
+++ techcare/src/Components/Navbar.jsx
@@ -1,10 +1,44 @@
 // import React from 'react'
-
 const Navbar = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div
+      id="nav-container"
+      className="d-flex align-items-center justify-content-between gap- bg-body-white px-4 py-2 shadow-sm rounded-5 mx-4 my-3"
+    >
+      <div id="logo">
+        <div>
+          <img src="TestLogo.svg" alt="" width={160} />
+        </div>
+      </div>
+      <div id="nav" className="d-flex gap-3 justify-content-between">
+        <div id="nav-links" className="d-flex p-1 gap-1">
+          <img src="home.svg" alt="" width={14}/>
+          <span className="fw-bold">Overview</span>
+        </div>
+        <div id="nav-links" className="d-flex p-1 gap-1">
+          <img src="group.svg" alt="" width={20}/>
+          <span className="fw-bold">Patients</span>
+        </div>
+        <div id="nav-links" className="d-flex p-1 gap-1">
+          <img src="calendar.svg" alt="" width={13}/>
+          <span className="fw-bold">Schedule</span>
+        </div>
+        <div id="nav-links" className="d-flex p-1 gap-1">
+          <img src="chat.svg" alt="" width={15}/>
+          <span className="fw-bold">Message</span>
+        </div>
+        <div id="nav-links" className="d-flex p-1 gap-1">
+          <img src="card.svg" alt="" width={15}/>
+          <span className="fw-bold">Transactions</span>
+        </div>
+      </div>
+      <div id="right">
+        <div>
+          <img src="TestLogo.svg" alt="" width={150} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
