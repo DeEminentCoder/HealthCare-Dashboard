3ab50bfdef0e88bb7dcd714d5f691b9813a159c1
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-12-20 17:47:48 +0100

    Active button

--- techcare/src/Components/Navbar.jsx
+++ techcare/src/Components/Navbar.jsx
@@ -10,31 +10,44 @@ const Navbar = () => {
           <img src="TestLogo.svg" alt="" width={160} />
         </div>
       </div>
-      <div id="nav" className="d-flex gap-4 justify-content-between">
-        <div id="nav-links" className="d-flex p-1 gap-1">
-          <img src="home.svg" alt="" width={14}/>
-          <span className="fw-bold">Overview</span>
+      <div id="nav" className="d-flex gap-2 justify-content-between">
+        <div id="nav-links" className="d-flex py-1 px-3 btn">
+          <img src="home.svg" alt="" width={14} />
+          <span className="fw-bold p-1">Overview</span>
         </div>
-        <div id="nav-links" className="d-flex p-1 gap-1">
-          <img src="group.svg" alt="" width={20}/>
-          <span className="fw-bold">Patients</span>
+        <div
+          id="nav-links"
+          className="d-flex py-1 px-3 btn btn-info active rounded-5"
+        >
+          <img src="group.svg" alt="" width={20} />
+          <span className="fw-bold p-1">Patients</span>
         </div>
-        <div id="nav-links" className="d-flex p-1 gap-1">
-          <img src="calendar.svg" alt="" width={13}/>
-          <span className="fw-bold">Schedule</span>
+        <div id="nav-links" className="d-flex py-1 px-3 btn">
+          <img src="calendar.svg" alt="" width={13} />
+          <span className="fw-bold p-1">Schedule</span>
         </div>
-        <div id="nav-links" className="d-flex p-1 gap-1">
-          <img src="chat.svg" alt="" width={15}/>
-          <span className="fw-bold">Message</span>
+        <div id="nav-links" className="d-flex py-1 px-3 btn">
+          <img src="chat.svg" alt="" width={15} />
+          <span className="fw-bold p-1">Message</span>
         </div>
-        <div id="nav-links" className="d-flex p-1 gap-1">
-          <img src="card.svg" alt="" width={15}/>
-          <span className="fw-bold">Transactions</span>
+        <div id="nav-links" className="d-flex py-1 px-3 btn">
+          <img src="card.svg" alt="" width={15} />
+          <span className="fw-bold p-1">Transactions</span>
         </div>
       </div>
-      <div id="right">
-        <div>
-          <img src="TestLogo.svg" alt="" width={150} />
+      <div id="right" className="d-flex">
+        <div id="right-links" className="d-flex gap-1 btn">
+          <img src="senior.png" alt="" height={35} />
+          <div className="d-flex flex-column text-start">
+            <span className="fw-bold p-0">Dr. Jose Simmons</span>
+            <span className="fw- p-0">General Practitioner</span>
+          </div>
+        </div>
+        <div id="" className="btn">
+          <img src="settings.svg" alt="" width={15} />
+        </div>
+        <div id="nav-links" className="btn">
+          <img src="more.svg" alt="" width={3} />
         </div>
       </div>
     </div>
