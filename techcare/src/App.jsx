4be55b6f27ed4d58288fd9ce52087d5778eb5a95
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-12-21 11:16:46 +0100

    Add Herobody component and update Navbar for improved layout and accessibility

--- techcare/src/App.jsx
+++ techcare/src/App.jsx
@@ -4,11 +4,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar"
+import Herobody from "./Components/Herobody";
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
+      <Herobody></Herobody>
     </>
   )
 }
