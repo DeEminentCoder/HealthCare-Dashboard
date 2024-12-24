a71fc071f81e3ae1df924bfc54838f9d7b96e5cd
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-12-24 12:51:40 +0100

    Refactor Herobody component for improved layout and add font family to global styles

--- techcare/src/Components/Herobody.jsx
+++ techcare/src/Components/Herobody.jsx
@@ -99,7 +99,7 @@ const Herobody = () => {
         <div className="col-md-3">
           <div
             className="bg-white rounded py-3 shadow overflow-auto"
-            style={{ height: "850px" }}
+            style={{ height: "825px" }}
           >
             <div className="d-flex justify-content-between align-items-center mb-3  px-3">
               <h5 className="card-title mb-0 fw-bold">Patients</h5>
@@ -135,7 +135,7 @@ const Herobody = () => {
             style={{ height: "550px" }}
           >
             <h5 className="card-title mb-3 fw-bold">Diagnosis History</h5>
-            <div className="d-flex justify-content-center mb-">
+            <div className="d-flex justify-content-center">
               <Line options={options} data={data} height={130} />
               <br />
             </div>
@@ -165,40 +165,90 @@ const Herobody = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="card">
-                  <div className="card-body bg-danger bg-opacity-10">
-                    <h5 className="card-title">
-                      <img src="HeartBPM.svg" alt="" width={70} />
-                    </h5>
-                    <small className="card-text">Heart rate</small>
-                    <p className="card-text-bold fw-bold fs-5">78 bpm</p>
-                    <small className="norm">▾ Lower than average</small>
-                  </div>
-                </div>
-              </div>
+  <div className="card">
+    <div className="card-body bg-danger bg-opacity-10">
+      <h5 className="card-title">
+        <img src="HeartBPM.svg" alt="" width={70} />
+      </h5>
+      <small className="card-text">Heart rate</small>
+      <p className="card-text-bold fw-bold fs-5">78 bpm</p>
+      <div className="dropdown dropstart">
+        <small
+          className="dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <small className="norm">Lower than average</small>
+        </small>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li className="">
+            <span className="dropdown-item text-muted" href="#">
+              View Details
+            </span>
+          </li>
+          <li>
+            <span className="dropdown-item text-muted" href="#">
+            View Details
+            </span>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
             </div>
           </div>
           <div
-            className="bg-white rounded mt-5 p-3 shadow overflow-auto"
-            style={{ height: "250px" }}
-          >
-            <h5 className="card-title mb-3 fw-bold">Diagnosis List</h5>
-            <div className="d-flex justify-content-between bg-light rounded-5 px-3">
-              <div className="fw-medium"><small>Problem/Diagnostsis</small></div>
-              <div className="fw-medium"><small>Description</small></div>
-              <div className="fw-medium"><small>Status</small></div>
-            </div>
-            <div className="col-md-4 p-3">
-                <div className="card">
-                  <div className="card-body bg-danger bg-opacity-10">
-                    
-                    <small className="card-text">Heart rate</small>
-                    <p className="card-text-bold fw-bold fs-5">78 bpm</p>
-                    <small className="norm">▾ Lower than average</small>
-                  </div>
-                </div>
-              </div>
-          </div>
+  className="bg-white rounded mt-4 p-3 shadow overflow-auto"
+  style={{ height: "250px" }}
+>
+  <h5
+    className="mb-4 fw-bold"
+  >
+    Diagnosis List
+  </h5>
+  <table className="table table-borderless">
+    <thead className="norm table-light rounded-5">
+      <tr>
+        <th className="text-start">Problem/Diagnosis</th>
+        <th className="text-start ">Description</th>
+        <th className="text-start">Status</th>
+      </tr>
+    </thead>
+    <tbody className="text-muted">
+      <tr>
+        <td className="text-start pt-4">Hypertension</td>
+        <td className="text-start pt-4">High blood pressure condition</td>
+        <td className="text-start pt-4 text-nowwrap text-break lh-sm">Under <br /> Observation</td>
+      </tr>
+      <tr>
+        <td className="text-start">Type 2 Diabetes</td>
+        <td className="text-start">Chronic high blood pressure</td>
+        <td className="text-start">Cured</td>
+      </tr>
+      <tr>
+        <td className="text-start">Asthma</td>
+        <td className="text-start">Managed Managed</td>
+        <td className="text-start">Cured</td>
+      </tr>
+      <tr>
+        <td className="text-start">Asthma</td>
+        <td className="text-start">Managed Managed</td>
+        <td className="text-start">Cured</td>
+      </tr>
+      <tr>
+        <td className="text-start">Asthma</td>
+        <td className="text-start">Managed Managed</td>
+        <td className="text-start">Cured</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
         </div>
 
         <div className="col-md-3 pb-5">
@@ -212,53 +262,101 @@ const Herobody = () => {
             </center>
             <div>
               <div className="p-2 d-flex align-items-center">
-                <div className="pe-4"><img src="InsuranceIcon.svg" alt="" width={30}/></div>
+                <div className="pe-4">
+                  <img src="InsuranceIcon.svg" alt="" width={30} />
+                </div>
                 <div className="selected-info">
-                  <small>Date of Birth</small><br />
+                  <small>Date of Birth</small>
+                  <br />
                   <small className="fw-bold">August 92, 1996</small>
                 </div>
               </div>
               <div className="p-2 d-flex align-items-center">
-                <div className="pe-4"><img src="FemaleIcon.svg" alt="" width={30}/></div>
+                <div className="pe-4">
+                  <img src="FemaleIcon.svg" alt="" width={30} />
+                </div>
                 <div className="selected-info">
-                  <small>Gender</small><br />
+                  <small>Gender</small>
+                  <br />
                   <small className="fw-bold">Female</small>
                 </div>
               </div>
               <div className="p-2 d-flex align-items-center">
-                <div className="pe-4"><img src="PhoneIcon.svg" alt="" width={30}/></div>
+                <div className="pe-4">
+                  <img src="PhoneIcon.svg" alt="" width={30} />
+                </div>
                 <div className="selected-info">
-                  <small>Contact Info.</small><br />
+                  <small>Contact Info.</small>
+                  <br />
                   <small className="fw-bold">+234 810 373 9480</small>
                 </div>
               </div>
               <div className="p-2 d-flex align-items-center">
-                <div className="pe-4"><img src="PhoneIcon.svg" alt="" width={30}/></div>
+                <div className="pe-4">
+                  <img src="PhoneIcon.svg" alt="" width={30} />
+                </div>
                 <div className="selected-info">
-                  <small>Emergency Contact</small><br />
+                  <small>Emergency Contact</small>
+                  <br />
                   <small className="fw-bold">+234 810 373 9480</small>
                 </div>
               </div>
               <div className="p-2 d-flex align-items-center">
-                <div className="pe-4"><img src="InsuranceIcon.svg" alt="" width={30}/></div>
+                <div className="pe-4">
+                  <img src="InsuranceIcon.svg" alt="" width={30} />
+                </div>
                 <div className="selected-info">
-                  <small>Insurance Provider</small><br />
+                  <small>Insurance Provider</small>
+                  <br />
                   <small className="fw-bold">Sunrise Health Assurance</small>
                 </div>
               </div>
             </div>
             <center className="">
-              <button className="btn btn-sm btn-info rounded-5 fw-medium mt-3 px-4">Show all information</button>
+              <button className="btn btn-sm btn-info rounded-5 fw-medium mt-3 px-4">
+                Show all information
+              </button>
             </center>
           </div>
           <div
             className="bg-white mt-5 rounded p-3 shadow overflow-auto"
-            style={{ height: "240px" }}
+            style={{ height: "215px" }}
           >
-            <h5 className="mb-3">Patient Details</h5>
-            <div className="alert alert-info">
-              Highlighted Patient Information
-            </div>
+            <h5 className="mb-3 fw-bold">Lab Results</h5>
+            <div className="d-block">
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Blood Test</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3 rounded-2 bg-light">
+                  <span>CT Scans</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Hypertension</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>X-Rays</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Hypertension</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Hypertension</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Hypertension</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+                <div className="norm py-2 text-muted d-flex justify-content-between px-3">
+                  <span>Hypertension</span>
+                  <img src="download_FILL0_wght300_GRAD0_opsz24 (1).svg" alt="" width={15}/>
+                </div>
+              </div>
           </div>
         </div>
       </div>
