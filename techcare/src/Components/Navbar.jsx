ce039aedbb9ac8383c08259a4f4e49e648a99178
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-12-26 14:52:16 +0100

    Refactor Navbar component for improved structure and consistency in styling

--- techcare/src/Components/Navbar.jsx
+++ techcare/src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
-// import React from 'react'
+// import React from "react";
+
 const Navbar = () => {
   return (
     <div
@@ -6,55 +7,30 @@ const Navbar = () => {
       className="d-flex flex-wrap align-items-center justify-content-md-between justify-content-center bg-body-white px-4 py-2 shadow-sm rounded-5 mx-2 my-2"
     >
       <div id="logo" className="mb-3 mb-md-0 text-md-start text-center">
-        <img
-          src="TestLogo.svg"
-          alt="Logo"
-          className="img-fluid"
-          style={{ maxWidth: '150px' }}
-        />
+        <img src="TestLogo.svg" alt="Logo" className="img-fluid" />
       </div>
 
       <div
         id="nav"
         className="d-flex gap-2 justify-content-center align-items-center flex-wrap"
       >
-        <div
-          id="nav-links"
-          className="d-flex py-1 px-2 btn"
-          style={{ fontSize: '0.9rem' }}
-        >
+        <div className="nav-links d-flex py-1 px-2 btn">
           <img src="home.svg" alt="Home" width={12} />
           <span className="fw-bold p-1">Overview</span>
         </div>
-        <div
-          id="nav-links"
-          className="d-flex py-1 px-2 btn btn-outline-0 rounded-5"
-          style={{ fontSize: '0.9rem',backgroundColor: '#01F0D0' }}
-        >
+        <div className="nav-links d-flex py-1 px-2 btn btn-outline-0 rounded-5 patientBtn">
           <img src="group.svg" alt="Group" width={16} />
           <span className="fw-bold p-1">Patients</span>
         </div>
-        <div
-          id="nav-links"
-          className="d-flex py-1 px-2 btn"
-          style={{ fontSize: '0.9rem' }}
-        >
+        <div className="nav-links d-flex py-1 px-2 btn">
           <img src="calendar.svg" alt="Calendar" width={11} />
           <span className="fw-bold p-1">Schedule</span>
         </div>
-        <div
-          id="nav-links"
-          className="d-flex py-1 px-2 btn"
-          style={{ fontSize: '0.9rem' }}
-        >
+        <div className="nav-links d-flex py-1 px-2 btn">
           <img src="chat.svg" alt="Chat" width={12} />
           <span className="fw-bold p-1">Message</span>
         </div>
-        <div
-          id="nav-links"
-          className="d-flex py-1 px-2 btn"
-          style={{ fontSize: '0.9rem' }}
-        >
+        <div className="nav-links d-flex py-1 px-2 btn">
           <img src="card.svg" alt="Transactions" width={12} />
           <span className="fw-bold p-1">Transactions</span>
         </div>
@@ -64,11 +40,7 @@ const Navbar = () => {
         id="right"
         className="d-flex align-items-center justify-content-md-end justify-content-center gap-2 flex-wrap mt-2 mt-md-0"
       >
-        <div
-          id="right-links"
-          className="d-flex align-items-center gap-2 btn flex-wrap justify-content-center text-center"
-          style={{ fontSize: '0.85rem' }}
-        >
+        <div id="right-links" className="d-flex align-items-center gap-2 btn flex-wrap justify-content-center text-center">
           <img src="senior.png" alt="Doctor" height={30} />
           <div className="d-flex flex-column text-center text-md-start">
             <span className="fw-bold p-0">Dr. J. Simmons</span>
@@ -79,7 +51,7 @@ const Navbar = () => {
           <img src="settings.svg" alt="Settings" width={12} />
         </div>
         <div id="more" className="btn ps-0">
-          <img src="more.svg" alt="More" width={2} />
+          <img src="more.svg" alt="Simeon" width={2} />
         </div>
       </div>
     </div>
