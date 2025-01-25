8462d7713d4776a25fc0301bce4175356ce37165
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2025-01-25 13:38:05 +0100

    CLEAR

--- techcare/src/App.jsx
+++ techcare/src/App.jsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Components/Navbar"
 import Herobody from "./Components/Herobody";
 
 const App = () => {
